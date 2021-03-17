f4b17541e820bbf02d76d90f09505dc2c5a72c49
true62 <80831331+true62@users.noreply.github.com>
2021-03-17 13:07:54 +0000

    Shellport

---
+++ windows.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "peepo";
+  return 0;
+}
+// unused stuff goes here
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  cout << "Finished port.";
+  return 0;
+}
