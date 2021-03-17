c7392c184b2abd07e88cfef58c208be711eb2f9c
true62 <80831331+true62@users.noreply.github.com>
2021-03-17 13:05:52 +0000

    Port (test edition)

---
+++ port.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# This is a comment!
+echo "Porting"       # This is a comment, too!
+echo "Porting$ chmod 755 first.sh
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
+$ chmod 755 first.sh
+$ ./first.sh
+Hello World
+$
