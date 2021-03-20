e85a15497ca4cb30763cfdf038712be99edd20c5
ak47 <80831331+true62@users.noreply.github.com>
2021-03-20 20:50:27 +0000

    Create shellport.bat

---
+++ shellport.bat
@@ -0,0 +1,7 @@
+echo S-H-E-L-L-P-O-R-T
+echo Porting.. 
+echo Port is unfinished due to human verify was expired as a token, retrying in 200 seconds...
+pause
+echo Shellport was shut down due to invaild human token.
+echo You may exit now
+pause
